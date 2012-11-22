4df347952975c7c2c6ea70f25dd6521c6616528e
labocho <labocho@penguinlab.jp>
2012-11-22 11:44:43 +0900

    Caching Handlebars::Context

--- lib/sht_rails/handlebars.rb
+++ lib/sht_rails/handlebars.rb
@@ -7,15 +7,18 @@ module ShtRails
     def self.call(template)
       if template.locals.include?(ShtRails.action_view_key.to_s) || template.locals.include?(ShtRails.action_view_key.to_sym)
 <<-SHT
-  hbs_context_for_sht = Handlebars::Context.new
   partials.each do |key, value|
-    hbs_context_for_sht.register_partial(key, value)
+    ::ShtRails::Handlebars.context.register_partial(key, value)
   end if defined?(partials) && partials.is_a?(Hash)
-  hbs_context_for_sht.compile(#{template.source.inspect}).call(#{ShtRails.action_view_key.to_s} || {}).html_safe
+  ::ShtRails::Handlebars.context.compile(#{template.source.inspect}).call(#{ShtRails.action_view_key.to_s} || {}).html_safe
 SHT
       else
         "#{template.source.inspect}.html_safe"
       end
+    end
+
+    def self.context
+      @context ||= ::Handlebars::Context.new
     end
   end
 end
